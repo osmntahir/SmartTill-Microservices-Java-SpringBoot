60b3c029a633a7f37610b540583ae0244adf9d3b
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-06-13 17:17:57 +0300

    changed Date field in Sale table

--- sale-service/src/main/java/com/toyota/saleservice/dto/SaleDto.java
+++ sale-service/src/main/java/com/toyota/saleservice/dto/SaleDto.java
@@ -2,13 +2,15 @@ package com.toyota.saleservice.dto;
 
 import com.toyota.saleservice.domain.PaymentType;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+
+import java.time.LocalDateTime;
+
 import java.util.List;
 
 @Data
@@ -18,7 +20,7 @@ public class SaleDto {
     //private Long userId;
     private Long id;
     @NotNull(message = "Creation date must be not null")
-    private Date creationDate;
+    private LocalDateTime creationDate;
     @NotNull(message = "Total price must be not null")
     private PaymentType paymentType;
     @Valid

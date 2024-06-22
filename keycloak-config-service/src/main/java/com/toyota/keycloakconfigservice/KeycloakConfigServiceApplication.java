8e94d955a9b41e51b8aae7e33c837876125a0036
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-06-22 17:57:56 +0300

    added keycloak configuration service

---
+++ keycloak-config-service/src/main/java/com/toyota/keycloakconfigservice/KeycloakConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toyota.keycloakconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakConfigServiceApplication.class, args);
+    }
+
+}
---
+++ keycloak-config-service/src/test/java/com/toyota/keycloakconfigservice/KeycloakConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyota.keycloakconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

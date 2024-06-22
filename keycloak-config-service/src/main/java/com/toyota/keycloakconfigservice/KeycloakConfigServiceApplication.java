73067777fab846032f5a5a9f4261abe6a4e18184
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-06-22 21:00:47 +0300

    added some configuration example  for keycloak-config-service

--- keycloak-config-service/src/main/java/com/toyota/keycloakconfigservice/KeycloakConfigServiceApplication.java
+++ keycloak-config-service/src/main/java/com/toyota/keycloakconfigservice/KeycloakConfigServiceApplication.java
@@ -2,6 +2,9 @@ package com.toyota.keycloakconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class KeycloakConfigServiceApplication {
@@ -10,4 +13,8 @@ public class KeycloakConfigServiceApplication {
         SpringApplication.run(KeycloakConfigServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return new RestTemplate();
+    }
 }

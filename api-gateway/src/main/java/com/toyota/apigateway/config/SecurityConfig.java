3fe3b91e5349eebd11ec968e31c93ca4cbd81b4b
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-09-04 13:07:40 +0300

    Integrate JWT-based authentication and role-based access with Keycloak

---
+++ api-gateway/src/main/java/com/toyota/apigateway/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.toyota.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**").permitAll()  // Allow access to Eureka
+                        .pathMatchers("/product/**").hasRole("ADMIN")  // Role-based access for ADMIN
+                        .pathMatchers("/user/**").hasRole("USER")    // Role-based access for USER
+                        .anyExchange().authenticated()               // All other requests must be authenticated
+                )
+                .oauth2ResourceServer((oauth) -> oauth
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(new CustomJwtAuthenticationConverter())) // Custom converter for roles
+                )
+                .build();
+    }
+
+
+}
